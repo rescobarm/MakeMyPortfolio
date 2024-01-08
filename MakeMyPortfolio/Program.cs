f0afb29eaa01c2c77ba1c0b6f9fbe7694439bdb0
rescobarm <rudyard.matuz@hotmail.com>
2024-01-08 00:54:41 -0600

    go do in my sky, some

--- MakeMyPortfolio/Program.cs
+++ MakeMyPortfolio/Program.cs
@@ -11,4 +11,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddFluentUIComponents();
 
+//LibraryConfiguration config = new(ConfigurationGenerator.GetIconConfiguration(), ConfigurationGenerator.GetEmojiConfiguration());
+//builder.Services.AddFluentUIComponents(config);
+
 await builder.Build().RunAsync();
